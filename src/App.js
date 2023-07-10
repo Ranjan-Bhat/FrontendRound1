ba128482b7a3787269707b182fd99d04549c2156
Ranjan Bhat <109158234+Ranjan-Bhat@users.noreply.github.com>
2023-07-10 17:50:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import { Posts } from "./components/Posts";
+export default function App() {
+  return (
+    <div className="App">
+      <Posts />
+    </div>
+  );
+}
---
+++ src/components/Posts.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+
+export const Posts = () => {
+  const [title, setTitle] = useState("");
+  const [array, setArray] = useState([]);
+  const [filteredArray, setFilteredArray] = useState([]);
+
+  useEffect(() => {
+    async function fetchTitleInfo() {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const json = await res.json();
+        setArray(json);
+        console.log("json is ", json);
+      } catch (error) {
+        console.log("error is ", error);
+      }
+    }
+    fetchTitleInfo();
+  }, []);
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleClick = () => {
+    const filterArray = array.filter((item) => item.title.includes(title));
+    setFilteredArray(filterArray);
+  };
+
+  return (
+    <>
+      <h1>Posts</h1>
+      <input
+        type="text"
+        placeholder="Enter title name..."
+        value={title}
+        onChange={handleChange}
+      />
+      <div>
+        <button onClick={handleClick}>Search Titles</button>
+      </div>
+      <ul>
+        {filteredArray.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
